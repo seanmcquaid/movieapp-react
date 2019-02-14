84796d510464556f6345b4cf20524449a825c82a
seanmcquaid <sean.mcquaid2@verizon.net>
2019-02-14 14:36:18 -0500

    added search

--- src/App.js
+++ src/App.js
@@ -9,7 +9,27 @@ class App extends Component {
     this.state = {
       movieList: []
     }
+    this.movieSearch = this.movieSearch.bind(this)
   }
+
+  movieSearch(event){
+    event.preventDefault();
+    console.log("form submitted")
+    const movieTitle = document.getElementById("searchTerm").value;
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=fec8b5ab27b292a68294261bb21b04a5&query='+movieTitle;
+		fetch(url)
+		.then((response)=>{
+		  return response.json();
+		})
+		.then((myJson)=>{
+			const results = myJson.results;
+			console.log(results)
+			this.setState({
+				movieList: results
+			});
+		});		
+  }
+
   componentDidMount(){
         const url = "https://api.themoviedb.org/3/movie/now_playing?api_key=fec8b5ab27b292a68294261bb21b04a5";
         
@@ -38,6 +58,12 @@ class App extends Component {
       <div className="container">
         <div className="row">
         <h1>The Movie App for the 5th Time</h1>
+
+        <form onSubmit={this.movieSearch}>
+          <input id="searchTerm" type="text" placeholder="Movie Title"/>
+          <button type="submit" className="btn">Submit</button>
+        </form>
+
         {posters}
         </div>
       </div>
